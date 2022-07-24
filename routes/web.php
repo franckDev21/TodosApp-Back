62c90163ee56a825a36cc1ec2d65484c31e02cab
franckDev21 <tutodevphp@gmail.com>
2022-07-24 05:54:13 +0100

    update app

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $users = User::with(['todoLists'])->get();
     // $user = User::with(['todoLists'])->find(1);
-
+    
     return view('welcome',compact('users'));
 });
